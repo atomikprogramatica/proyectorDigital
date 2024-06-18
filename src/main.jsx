f7e6873dba126df095ad43ceddd59a2752285a14
Andy Uzal <andyuzal@gmail.com>
2024-06-18 11:32:51 -0300

    Change mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
