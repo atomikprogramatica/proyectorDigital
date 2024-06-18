6589d2e92aef3c2d4b0794feb8c25500eb79efc2
Andy Uzal <andyuzal@gmail.com>
2024-06-18 11:36:43 -0300

    Ch

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import { ReactDom } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
+
+ReactDom.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
