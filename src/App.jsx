58a8eacd924521c4fedb042cee1ae1fb68d96b18
Andy Uzal <andyuzal@gmail.com>
2024-06-18 12:57:09 -0300

    ultimo cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './index.css'
 import SeccionFormatos from './Components/Formatos/Index'
 import Home from './Components/Home/Index'
@@ -6,13 +6,11 @@ import Home from './Components/Home/Index'
 function App() {
 
   return (
-    <BrowserRouter>
     <Routes>
       <Route  path='/' element={<Navigate to='./proyectorDigital' />} />
       <Route  path='/proyectorDigital' element={<Home />} />
       <Route  path='/proyectorDigital/ver-formatos' element={<SeccionFormatos />} /> 
     </Routes>
-    </BrowserRouter>
   )
 }
 
